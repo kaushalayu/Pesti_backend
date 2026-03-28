abade25f38c5df626313964e3c587bd3d4a9d1a6
kaushalayu <kaushalayush95@gmail.com>
2026-03-28 12:01:51 +0530

    done

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 
-const server = app.listen("/", () => {
+const server = app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
 
